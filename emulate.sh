0683ed3aa39743f82ab258d12f00d809dd187081
Nathanne Isip <nathanneisip@gmail.com>
2024-06-11 02:23:05 +0800

    Improved emulate shell script for live ISO file.

--- emulate.sh
+++ emulate.sh
@@ -1,7 +1,4 @@
 qemu-system-x86_64 \
     -m 512M \
     -vga std \
-    -drive file=nate_os.img,format=raw \
-    -nic user,hostfwd=tcp::8888-:22 \
-    -netdev user,id=n0 \
-    -device virtio-net-pci,netdev=n0
+    -cdrom nate_os.iso
